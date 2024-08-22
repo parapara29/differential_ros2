d2bc9e58de47533562bb73ceb58f536ee03970e4
parapara29 <shivanshc051@gmail.com>
2024-08-22 21:50:22 +0530

    added comments

--- diff_bot_description/launch/gazebo.launch.py
+++ diff_bot_description/launch/gazebo.launch.py
@@ -118,15 +118,14 @@ def generate_launch_description():
     {'use_sim_time': use_sim_time}])
 
     return LaunchDescription([
-        joint_broad_spawner,
-        diff_drive_spawner,
-        # joint_state_publisher_node,
-        gazebo_server,
-        gazebo_client,
-        robot_state_publisher_node,
-        urdf_spawn_node,
-        odom_node,
-        rviz_node,
-        # robot_localization
-        # rviz_node,
+        joint_broad_spawner, #The node managed by controller_manager
+        diff_drive_spawner, # Node for controlling the robot
+        # joint_state_publisher_node, # Commented out because we already have joint data by the joint_broad_spawner
+        gazebo_server, #gazebo node
+        gazebo_client, #gazebo node
+        robot_state_publisher_node, #Transformation of robot links node
+        urdf_spawn_node, 
+        odom_node, #Custom odom TF node
+        rviz_node, #RVIZ
+        # robot_localization # commented cause we have custom odom running
     ])
